372ca7eebb18478c66c9d0f327b1155849172ea9
= <=>
2024-11-13 14:58:49 +0200

    backend - creating a login route upon receiving a token and saving it to a cookie, in addition to adding an initial amount field to the user

--- Full stack final project/backend/src/controllers/authController.ts
+++ Full stack final project/backend/src/controllers/authController.ts
@@ -1,25 +1,46 @@
 import { Request, Response } from "express";
 import User from "../models/userModel";
+import Organization from "../models/organizationModel";
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 export const register = async (req: Request, res: Response): Promise<void> => {
 
     try {
         const { name, password, organization, area } = req.body;
+        const existingUser = await User.findOne({ name });
+        if (existingUser) {
+            res.status(400).json({ error: "User name already exists" });
+            return;
+        }
         if (!name || !organization || !password) {
             res.status(400).json({ error: "Missing required fields" });
             return;
         }
 
         
-
         const hashedPassword = await bcrypt.hash(password, 10);
+
+        const userOrganization = await Organization.findOne({ name: organization });
+        if (!userOrganization) {
+            res.status(400).json({ error: "Organization not found" });
+            return;
+        }
+
+        const budget = userOrganization.budget;
+        if (budget < 0) {
+            res.status(400).json({ error: "Budget cannot be negative" });
+            return;
+        }
+
 
         const user = new User({
             name,
             password: hashedPassword,
             organization,
-            area    
+            area,
+            budget
+             
         });
 
         await user.save();
@@ -32,5 +53,47 @@ export const register = async (req: Request, res: Response): Promise<void> => {
         res.status(500).json({ error: "Failed to save user" });
         return
 
+    }
+};
+
+
+export const login = async (req: Request, res: Response): Promise<void> => {
+    try {
+
+        const { name, password } = req.body;
+
+        if (!name || !password) {
+            res.status(400).json({ error: "Missing required fields" });
+            return;
+        }
+
+        const user = await User.findOne({ name });
+
+        if (!user) {
+            res.status(401).json({ error: "Invalid email or password" });
+            return
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            res.status(401).json({ error: "Invalid email or password" });
+            return
+        }
+
+        const token = jwt.sign({ id: user._id, organization: user.organization }, process.env.JWT_SECRET || "", { expiresIn: "5h" });
+
+        res.cookie("auth_token", token, {
+            maxAge: 1000*60*60*5,   
+            httpOnly: true,
+            sameSite: 'strict'
+        });
+
+        res.json({ message: "Login successful", token });
+        return
+
+    } catch (error) {
+
+        console.error("Error logging in:", error);
+        res.status(500).json({ error: "Failed to login" });
     }
 };
--- Full stack final project/backend/src/models/userModel.ts
+++ Full stack final project/backend/src/models/userModel.ts
@@ -5,15 +5,17 @@ export interface IUser extends Document {
    password: string;
    organization:  string;
    area: string;
+   budget: number
 }
 
 export interface IUserModel extends mongoose.Model<IUser> {}
 
 const userSchema = new mongoose.Schema({
-   name: { type: String, required: true },
+   name: { type: String, required: true, unique: true },
    password: { type: String, required: true },
    organization: { type: String, required: true },
    area: { type: String },
+   budget: { type: Number },
 });
 
 export default mongoose.model<  IUser, IUserModel>("User", userSchema);
