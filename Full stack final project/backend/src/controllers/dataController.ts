4a729ad4fade5393dc6aa3e5200afc62c3e8f9ec
= <=>
2024-11-13 15:29:33 +0200

    backend - creating a route to receive all the weapons the organization has

--- Full stack final project/backend/src/controllers/dataController.ts
+++ Full stack final project/backend/src/controllers/dataController.ts
@@ -1,4 +1,5 @@
 import Missille from "../models/missillesModel";
+import Organization from "../models/organizationModel";
 import { Request, Response } from "express";
 
 export const getAllMissiles = async (req: Request, res: Response): Promise<void> => {
@@ -9,6 +10,24 @@ export const getAllMissiles = async (req: Request, res: Response): Promise<void>
 
     } catch (error) {
 
+        res.status(500).json({ error: "Failed to fetch missiles" });
+        return
+    }
+};
+
+export const getOrganizationMissiles = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const { id } = req.params;
+        const organization = await Organization.findById({ _id: id });
+        if (!organization) {
+            res.status(404).json({ error: "Organization not found" });
+            return
+        }
+        const missiles = organization.resources; 
+
+        res.status(200).json(missiles);
+        return
+    } catch (error) {
         res.status(500).json({ error: "Failed to fetch missiles" });
         return
     }
--- Full stack final project/backend/src/routes/dataRoutes.ts
+++ Full stack final project/backend/src/routes/dataRoutes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { getAllMissiles } from "../controllers/dataController";
+import { getAllMissiles, getOrganizationMissiles } from "../controllers/dataController";
 const authRoutes = Router();
 
 authRoutes.get("/missiles", getAllMissiles);
+authRoutes.get("/organizationMissiles/:id", getOrganizationMissiles);
+
 
 
 export default authRoutes;
